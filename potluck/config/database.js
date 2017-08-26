0b082eb263419c16e1da8050ffe1083a6eaeabf6
john solis <1johnmichael.solis@gmail.com>
2017-08-26 11:59:49 -0700

    starting code

---
+++ potluck/config/database.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect(process.env.DATABASE_URL)
+
+mongoose.connection.once('open', () => {
+    console.log(`Mongoose connected to ${process.env.DATABASE_URL}`)
+});
+
+mongoose.connection.once('error', (err) => {
+    console.log(`database error:\n${err}` )
+});
+
+module.exports = mongoose;
