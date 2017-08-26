aea5e0e89cce6af4c254fd798c0016af7b45333d
john solis <1johnmichael.solis@gmail.com>
2017-08-26 13:50:31 -0700

    event schema set up

--- potluck/models/user.js
+++ potluck/models/user.js
@@ -2,12 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 require('dotenv').config();
+let eventSchema = new Schema({
+    name:{type:String},
+    location:{type:String},
+    time:{type:Date}
+})
 
 let userSchema = new Schema({
     name: String,
     email: String,
     googleId: String,
-    photo: String
+    photo: String,
+    events:[eventSchema]
 }, {
     timestamps: true
 });
