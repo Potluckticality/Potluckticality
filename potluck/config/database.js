da8b918ec6bf235d203a10cc1e159e4a03ae3bcd
john solis <1johnmichael.solis@gmail.com>
2017-08-26 12:44:21 -0700

    started event model

--- potluck/config/database.js
+++ potluck/config/database.js
@@ -3,7 +3,7 @@ mongoose.Promise = Promise;
 mongoose.connect(process.env.DATABASE_URL)
 
 mongoose.connection.once('open', () => {
-    console.log(`Mongoose connected to ${process.env.DATABASE_URL}`)
+    console.log(`Mongoose connected to ${process.env.DATABASE_URL}`);
 });
 
 mongoose.connection.once('error', (err) => {
---
+++ potluck/models/event.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+var eventSchema = new eventSchema({
+
+})
+module.exports= mongoose.model('Event', eventSchema )
