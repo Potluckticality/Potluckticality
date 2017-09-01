8043399080eb43beca566f74f4f9d9c81df936c8
john solis <1johnmichael.solis@gmail.com>
2017-09-01 16:37:43 -0700

    fixing show on index

--- controllers/users.js
+++ controllers/users.js
@@ -116,8 +116,15 @@ function prepEmail(req,res) {
 }
 
 function homePage(req, res) {
+    let filteredEvents =[];
+    Event.find({}, function(err, event) {
+        if(event.users.includes(req.user.id) ) {
+            filteredEvents.push(event);
+        }
+    })
+    console.log(filteredEvents)
     User.populate(req.user, 'events', function(err) {
-        res.render('index', {user:req.user})
+        res.render('index', {user:req.user, events:fileteredEvents})
     });
 }
  
